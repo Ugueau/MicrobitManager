8c023927fde6c253de5e4e2b6f3763a63c125988
Hugo_Millot <hugomillot2003@gmail.com>
2025-05-22 16:40:25 +0200

    c

--- app/src/main/java/fr/cpe/microbitmanager/view/MicrobitConfigFragment.kt
+++ app/src/main/java/fr/cpe/microbitmanager/view/MicrobitConfigFragment.kt
@@ -6,37 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import fr.cpe.microbitmanager.R
 import androidx.recyclerview.widget.ItemTouchHelper
 import fr.cpe.microbitmanager.model.MicrobitInfo
+import fr.cpe.microbitmanager.viewmodel.MainViewModel
 
 class MicrobitConfigFragment(private val microbit : MicrobitInfo) : Fragment() {
+    private lateinit var mainViewModel: MainViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         return inflater.inflate(R.layout.fragment_microbit_config, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val recyclerView: RecyclerView = view.findViewById(R.id.microbit_config_recyclerview)
         val recyclerViewAdapter = MicrobitConfigAdapter()
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = recyclerViewAdapter
 
-        val microbit_name : TextView = view.findViewById<TextView>(R.id.microbit_config_name)
+        val microbit_name: TextView = view.findViewById(R.id.microbit_config_name)
         microbit_name.text = "${microbit.name}:${microbit.id}"
-        val microbit_order : TextView = view.findViewById<TextView>(R.id.microbit_config_order)
+        val microbit_order: TextView = view.findViewById(R.id.microbit_config_order)
         microbit_order.text = "Current configuration : ${microbit.formatConfig()}"
 
-
         val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
-            ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.START or ItemTouchHelper.END,
-            0
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN, 0
         ) {
             override fun onMove(
                 recyclerView: RecyclerView,
@@ -45,35 +48,37 @@ class MicrobitConfigFragment(private val microbit : MicrobitInfo) : Fragment() {
             ): Boolean {
                 val fromPosition = viewHolder.adapterPosition
                 val toPosition = target.adapterPosition
-                recyclerViewAdapter.notifyItemMoved(fromPosition, toPosition)
+                recyclerViewAdapter.onItemMove(fromPosition, toPosition)
                 return true
             }
 
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+                super.clearView(recyclerView, viewHolder)
+                viewHolder.itemView.alpha = 1f
 
+                val configOrder = recyclerViewAdapter.getCurrentOrder()
+
+                microbit.temperatureConfigIndex = configOrder.indexOf("temperature")
+                microbit.humidityConfigIndex = configOrder.indexOf("humidity")
+                microbit.luminosityConfigIndex = configOrder.indexOf("luminosity")
+
+                val microbit_order: TextView = view.findViewById(R.id.microbit_config_order)
+                microbit_order.text = "Current configuration : ${microbit.formatConfig()}"
             }
+
 
             override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
                 super.onSelectedChanged(viewHolder, actionState)
-                if(actionState == ItemTouchHelper.ACTION_STATE_DRAG)
-                {
+                if (actionState == ItemTouchHelper.ACTION_STATE_DRAG) {
                     viewHolder?.itemView?.alpha = 0.8f
                 }
             }
 
-            override fun clearView(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder
-            ) {
-                super.clearView(recyclerView, viewHolder)
-                viewHolder?.itemView?.alpha = 1f
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
             }
         }
 
         val itemTouchHelper = ItemTouchHelper(itemTouchHelperCallback)
         itemTouchHelper.attachToRecyclerView(recyclerView)
-
-
     }
-
 }
